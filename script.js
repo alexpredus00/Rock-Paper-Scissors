25f2540d9ef88d981e3a5ec783b347e061f4bc53
alexpredus00 <alexpredus@yahoo.com>
2022-08-12 01:04:16 +0300

    Added UI

--- script.js
+++ script.js
@@ -9,34 +9,35 @@ function computerPlay(){
 
 function playGame(playerSelection, computerSelection){
     let gameWinner = 1;   // 0 Draw, 1 player, 2 computer
+    const gameDetails = document.getElementById('resultDetails');
 
     if(playerSelection == 0 && computerSelection == 1){
-        console.log("Paper beats Rock! ~~ Computer Wins ~~")
+        gameDetails.textContent = "Paper beats Rock!    ~~ Computer Wins ~~";
         gameWinner = 2;
     } else if(playerSelection == 0 && computerSelection == 2){
-        console.log("Rock beats Scissor! ~~ Player Wins ~~")
+        gameDetails.textContent = "Rock beats Scissor!    ~~ Player Wins ~~";
         gameWinner = 1;
     } else if(playerSelection == 1 && computerSelection == 0){
-        console.log("Paper beats Rock! ~~ Player Wins ~~")
+        gameDetails.textContent = "Paper beats Rock!    ~~ Player Wins ~~";
         gameWinner = 1;
     } else if(playerSelection == 1 && computerSelection == 2){
-        console.log("Scissor beats paper! ~~ Computer Wins ~~")
+        gameDetails.textContent = "Scissor beats paper!    ~~ Computer Wins ~~";
         gameWinner = 2;
     } else if(playerSelection == 2 && computerSelection == 0){
-        console.log("Rock beats Scissor! ~~ Computer Wins ~~")
+        gameDetails.textContent = "Rock beats Scissor!    ~~ Computer Wins ~~";
         gameWinner = 2;
     } else if(playerSelection == 2 && computerSelection == 1){
-        console.log("Scissor beats Paper! ~~ Player Wins ~~")
+        gameDetails.textContent = "Scissor beats Paper! ~~ Player Wins ~~";
         gameWinner = 1;
     } else {
-        console.log("Draw! The game will be replayed!")
-        gameWinner = playGame(playerPlay(), computerPlay());
+        gameDetails.textContent = 'Draw! Please pick again.'
+        gameWinner = 0;
     }
     return gameWinner;
 }
 
-function playerPlay(){
-    let playerMove = prompt("Enter your move (Rock/Paper/Scissor): ");
+function playerPlay(playerMove){
+
     let moveNumber = playerMove.toUpperCase();
 
     if(moveNumber == "rock".toUpperCase()){
@@ -54,24 +55,37 @@ function playerPlay(){
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
-function game(roundsNumber){
-  let playerScore = 0;
-  let computerScore = 0;
+function game(playerMove){
   let gameWinner;
-  for(i = 0; i < roundsNumber; i++){
-    gameWinner = playGame(playerPlay(), computerPlay());
+    gameWinner = playGame(playerPlay(playerMove), computerPlay());
     if(gameWinner == 1){
       playerScore++;
     } else if(gameWinner == 2){
       computerScore++;
     }
-}
-    console.log("\n\n~~~ Final Score ~~~\n")
-    console.log("Player ", playerScore, " - ", computerScore, " Computer")
+
+    const liveScore = document.getElementById('score');
+    if(computerScore >= 5) {
+        liveScore.textContent = "COMPUTER WINS";
+        return 0;
+    }
+    if(playerScore >= 5) {
+        liveScore.textContent = "PLAYER WINS";
+        return 0;
+    } 
+    
+    liveScore.textContent = playerScore + "  -  " + computerScore;
+
+
+
+
+
+
   
 }
 
 
-// console.log(playGame(playerPlay(), computerPlay()));
-game(5);
+
